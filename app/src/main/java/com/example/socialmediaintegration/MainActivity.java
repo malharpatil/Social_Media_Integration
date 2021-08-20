3126c84276f016a352980e522284d133a06b0197
Malhar <malharypatil4@gmail.com>
2021-08-20 23:14:21 +0530

    Completed Login and RegisterActivity

---
+++ app/src/main/java/com/example/socialmediaintegration/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.socialmediaintegration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView createNewAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN , WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        createNewAccount = findViewById(R.id.createNewAccount);
+
+
+        createNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+}
